1a4345da5f3f04f3d6d5b74a7609a090246ad3fa
Hugo <hugohoffmann04@gmail.com>
2019-09-25 16:02:24 -0300

    chore: config redux

---
+++ goBarber/src/store/modules/user/sagas.js
@@ -0,0 +1,27 @@
+import { Alert } from 'react-native';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import api from '~/services/api';
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+export function* updateProfile( { payload } ){
+    try {
+        const {name, email, avatar_id, ...rest } = payload.data;
+
+        // unir dois objetos
+        const profile = Object.assign(
+            {name, email, avatar_id}, 
+            rest.oldPassaword ? rest : {}
+        );
+
+        const response = yield call(api.put, 'users', profile);
+
+        Alert.alert('Sucesso', 'Perfil atualizado com sucesso');
+
+        yield put(updateProfileSuccess(response.data));
+    } catch (error) {
+        Alert.alert('Erro', 'Erro ao atualizar perfil!');
+        yield put(updateProfileFailure);        
+    }
+}
+export default all([
+takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)
+]);
